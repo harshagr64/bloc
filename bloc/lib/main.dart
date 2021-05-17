1e1a045175dec50a34e55184edece3230c513faa
Harsh Agarwal <harshagr63@gmail.com>
2021-05-17 15:34:49 +0530

    Global Declaration of blocProvider

--- bloc/lib/main.dart
+++ bloc/lib/main.dart
@@ -4,33 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:bloc_tut/bloc.dart';
 import 'package:bloc/bloc.dart';
 import 'package:bloc_tut/presentaion/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
   @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: _appRouter.onGenerate,
       ),
-      onGenerateRoute: _appRouter.onGenerate,
     );
   }
 }
